a820202f8a3c7db99792598560261ba468c8aceb
ThirAthur <aththur909@gmail.com>
2026-03-20 19:31:03 +0700

    operasi array

--- Array.h
+++ Array.h
@@ -4,6 +4,14 @@
 #define ROW 256
 #define COL 256
 
+extern char text_buffer[ROW][COL];
+extern int row_pos;
+extern int col_pos;
+
 void insert_char(char text[ROW][COL], int *r, int *c, char ch);
+void delete_char(char text[ROW][COL], int *r, int *c);
+void indention(char text[ROW][COL], int *r, int *c);
+void array_checker(char ch);
+void new_line(char text[ROW][COL], int *r, int *c);
 
 #endif
