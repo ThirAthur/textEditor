e26a71768958a7906d220a6d8d95f5ce9d422dbf
M. Fachri Akbar Irfan <fachriakbar440@gmail.com>
2026-04-10 16:41:11 +0700

    update cursor

--- cursor.c
+++ cursor.c
@@ -1,12 +1,24 @@
 #include <string.h>
 #include "cursor.h"
 
-static int line_length(char text[ROW][COL], int row)
+static int get_document_max_row(char text[ROW][COL], int current_row) {
+    int max_row = current_row;
+    for (int i = 0; i < ROW; i++) {
+        if (text[i][0] != '\0') {
+            if (i > max_row) {
+                max_row = i;
+            }
+        }
+    }
+    return max_row;
+}
+
+long line_length(char text[ROW][COL], int row)
 {
     return strlen(text[row]);
 }
 
-static void shift_line_right(char text[ROW][COL], int row, int col)
+void shift_line_right(char text[ROW][COL], int row, int col)
 {
     int len = line_length(text, row);
 
@@ -19,7 +31,7 @@ static void shift_line_right(char text[ROW][COL], int row, int col)
     }
 }
 
-static void shift_line_left(char text[ROW][COL], int row, int col)
+void shift_line_left(char text[ROW][COL], int row, int col)
 {
     int len = line_length(text, row);
 
@@ -28,7 +40,7 @@ static void shift_line_left(char text[ROW][COL], int row, int col)
     }
 }
 
-static void insert_empty_line(char text[ROW][COL], int row)
+void insert_empty_line(char text[ROW][COL], int row)
 {
     for (int i = ROW - 1; i > row; i--) {
         strcpy(text[i], text[i - 1]);
@@ -36,7 +48,7 @@ static void insert_empty_line(char text[ROW][COL], int row)
     text[row][0] = '\0';
 }
 
-static void delete_line(char text[ROW][COL], int row)
+void delete_line(char text[ROW][COL], int row)
 {
     for (int i = row; i < ROW - 1; i++) {
         strcpy(text[i], text[i + 1]);
@@ -44,93 +56,7 @@ static void delete_line(char text[ROW][COL], int row)
     text[ROW - 1][0] = '\0';
 }
 
-void cursor_insert_char(char text[ROW][COL], int *row, int *col, char ch)
-{
-    if (*row < 0 || *row >= ROW) {
-        return;
-    }
-
-    if (*col < 0) {
-        *col = 0;
-    }
-
-    if (*col > line_length(text, *row)) {
-        *col = line_length(text, *row);
-    }
-
-    if (line_length(text, *row) >= COL - 1) {
-        return;
-    }
-
-    shift_line_right(text, *row, *col);
-    text[*row][*col] = ch;
-    (*col)++;
-}
-
-void cursor_backspace(char text[ROW][COL], int *row, int *col)
-{
-    if (*row < 0 || *row >= ROW) {
-        return;
-    }
-
-    if (*col > 0) {
-        shift_line_left(text, *row, *col - 1);
-        (*col)--;
-    } else if (*row > 0) {
-        cursor_merge_line(text, row, col);
-    }
-}
-
-void cursor_delete(char text[ROW][COL], int *row, int *col)
-{
-    int len;
-
-    if (*row < 0 || *row >= ROW) {
-        return;
-    }
-
-    len = line_length(text, *row);
-
-    if (*col < len) {
-        shift_line_left(text, *row, *col);
-    } else if (*row < ROW - 1 && text[*row + 1][0] != '\0') {
-        int current_len = line_length(text, *row);
-        int next_len = line_length(text, *row + 1);
-
-        if (current_len + next_len < COL) {
-            strcat(text[*row], text[*row + 1]);
-            delete_line(text, *row + 1);
-        }
-    }
-}
-
-void cursor_enter(char text[ROW][COL], int *row, int *col)
-{
-    int len;
-
-    if (*row < 0 || *row >= ROW - 1) {
-        return;
-    }
-
-    len = line_length(text, *row);
-
-    if (*col < 0) {
-        *col = 0;
-    }
-
-    if (*col > len) {
-        *col = len;
-    }
-
-    insert_empty_line(text, *row + 1);
-    strcpy(text[*row + 1], &text[*row][*col]);
-    text[*row][*col] = '\0';
-
-    (*row)++;
-    *col = 0;
-}
-
-void cursor_merge_line(char text[ROW][COL], int *row, int *col)
+void merge_line(char text[ROW][COL], int *row, int *col)
 {
     int prev_len;
     int curr_len;
@@ -169,9 +95,12 @@ void cursor_move_right(char text[ROW][COL], int *row, int *col)
 
     if (*col < len) {
         (*col)++;
-    } else if (*row < ROW - 1 && text[*row + 1][0] != '\0') {
-        (*row)++;
-        *col = 0;
+    } else {
+        int max_row = get_document_max_row(text, *row);
+        if (*row < max_row) {
+            (*row)++;
+            *col = 0;
+        }
     }
 }
 
@@ -187,7 +116,8 @@ void cursor_move_up(char text[ROW][COL], int *row, int *col)
 
 void cursor_move_down(char text[ROW][COL], int *row, int *col)
 {
-    if (*row < ROW - 1 && text[*row + 1][0] != '\0') {
+    int max_row = get_document_max_row(text, *row);
+    if (*row < max_row) {
         (*row)++;
         if (*col > line_length(text, *row)) {
             *col = line_length(text, *row);
