c6cde46b8982f5dee07eed4fea84c6441ec0f9f3
ThirAthur <aththur909@gmail.com>
2026-05-24 21:53:37 +0700

    array

--- cursor.h
+++ cursor.h
@@ -1,15 +1,11 @@
 #ifndef CURSOR_H
 #define CURSOR_H
 
-#include "Array.h"
+#include "Fathir.h"
 
-void shift_line_right(char text[ROW][COL], int row, int col);
-void shift_line_left(char text[ROW][COL], int row, int col);
-void insert_empty_line(char text[ROW][COL], int row);
-void merge_line(char text[ROW][COL], int *row, int *col);
-void cursor_move_left(char text[ROW][COL], int *row, int *col);
-void cursor_move_right(char text[ROW][COL], int *row, int *col);
-void cursor_move_up(char text[ROW][COL], int *row, int *col);
-void cursor_move_down(char text[ROW][COL], int *row, int *col);
+void cursor_move_left(List *L, address *poscursor, int *c);
+void cursor_move_right(List *L, address *poscursor, int *c);
+void cursor_move_up(List *L, address *poscursor, int *c);
+void cursor_move_down(List *L, address *poscursor, int *c);
 
 #endif
