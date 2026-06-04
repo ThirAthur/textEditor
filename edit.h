83391d07ae290aa06dcc2869fcf66b20f6e0ff5a
M. Fachri Akbar Irfan <fachriakbar440@gmail.com>
2026-06-05 01:06:48 +0700

    penambahan fitur undo redo

--- edit.h
+++ edit.h
@@ -8,7 +8,25 @@
 
 extern char internal_clipboard[CLIPBOARD_SIZE];
 
+typedef enum {
+    SESSION_NONE = 0,
+    SESSION_TYPING = 1,
+    SESSION_DELETE = 2
+} EditSession;
+
 void copy_selected_text(GtkTextView *text_view);
 void paste_clipboard_text(List *L, address *poscursor, int *col);
+
+void edit_init_history(void);
+void edit_clear_history(void);
+
+void edit_begin_typing(List *L, address poscursor, int col);
+void edit_begin_delete(List *L, address poscursor, int col);
+void edit_begin_single_action(List *L, address poscursor, int col);
+void edit_end_session(void);
+void edit_check_session_timeout(void);
+
+void edit_undo(List *L, address *poscursor, int *col);
+void edit_redo(List *L, address *poscursor, int *col);
 
 #endif
